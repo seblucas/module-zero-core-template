50e8b72d2ea2483f266dc33939cf25f95e9a3120
Sébastien Lucas <sebastien.lucas@quadra-diffusion.fr>
2019-03-19 17:38:56 +0100

    [test] add another testcase for the CustomValidation call discrepancy

---
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Application/Test/AnotherAppService.cs
@@ -0,0 +1,29 @@
+﻿using Abp.Application.Services;
+using Castle.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbpCompanyName.AbpProjectName.Test
+{
+	public class AnotherAppService : ApplicationService
+	{
+		private readonly ILogger _logger;
+		private readonly TestAppService _testAppService;
+
+		public AnotherAppService(TestAppService testAppService)
+		{
+			_logger = NullLogger.Instance;
+			_testAppService = testAppService;
+		}
+
+		public virtual int CallCreateThroughInjection()
+		{
+			_testAppService.Create(new TestCreateDto { Label = "Test" });
+			_logger.Info("It actually does not go there as the validator is really called");
+			return 1;
+		}
+	}
+}
